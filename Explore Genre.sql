3fa523f5c5caa6e64620ab871e2d8305be0c6fa3
Michelle Tanco <michelle.tanco@gmail.com>
2017-11-28 11:14:42 -0800

    Update Explore Genre.sql
    visualizations set up for app center

--- Explore Genre.sql
+++ Explore Genre.sql
@@ -1,5 +1,7 @@
+--name=base_data_cnt
 SELECT COUNT(*) FROM mt_movie_genres; --14,092
 
+--name=base_data_sample
 SELECT * FROM mt_movie_genres LIMIT 10;
 --movie_id	genre
 --1052		Adventure
@@ -13,8 +15,8 @@ SELECT * FROM mt_movie_genres LIMIT 10;
 --1578		Crime
 --1578		Drama
 
-SELECT genre, COUNT(*) FROM mt_movie_genres GROUP BY 1
-
+--name=genre_cnt
+SELECT genre, COUNT(*) FROM mt_movie_genres GROUP BY 1 ORDER BY 2 DESC;
 --genre			count(1)
 --Drama			2506
 --Comedy		1843
@@ -43,11 +45,12 @@ SELECT genre, COUNT(*) FROM mt_movie_genres GROUP BY 1
 --Reality-TV	2
 --Game-Show		1
 
-SELECT CNT, COUNT(*) FROM (
+--name=number_of_genres_per_film_trend
+SELECT CNT || ' Genres' AS genre_cnt, COUNT(*) FROM (
 	SELECT movie_id, COUNT(*) AS CNT 
 	FROM mt_movie_genres 
 	GROUP BY 1
-) AS X GROUP BY 1
+) AS X GROUP BY 1 ORDER BY 2 DESC;
 
 --cnt	count(1)
 --3		1582
@@ -59,6 +62,7 @@ SELECT CNT, COUNT(*) FROM (
 --7		18
 --8		4
 
+--name=genre_relationships
 SELECT 
 	'[' || A.genre || ',' || B.genre || ']' AS PATH
 	,COUNT(*) AS CNT
@@ -66,4 +70,4 @@ FROM mt_movie_genres AS A
 JOIN mt_movie_genres AS B
 	ON A.movie_id = B.movie_id
 	AND A.genre <> B.genre
-GROUP BY A.genre, B.genre
+GROUP BY A.genre, B.genre;
